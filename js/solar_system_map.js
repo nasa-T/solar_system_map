f0e8e07a3eefc4e1879be5cbf3f96543d2be5067
Tasan <tas2@williams.edu>
2023-01-23 16:53:52 -0500

    a lot of code that I do not know works

--- js/solar_system_map.js
+++ js/solar_system_map.js
@@ -26,7 +26,6 @@ function initializeCanvas() {
 
 function GET(date, time="00:00:00") {
     var timeFormatted = time.replace(/:/g, "%3A");
-    console.log(date);
     const settings = {
 	"async": true,
 	"crossDomain": true,
@@ -39,13 +38,104 @@ function GET(date, time="00:00:00") {
     };
 
     $.ajax(settings).done(function (response) {
-	console.log(response);
+	return response;
     });
 }
 
-function test() {
+function RAtoAngle(RA) {
+    return RA/24 * 360;
+}
+
+function earthFrameLoc(RA, dist) {
+    return {
+	x: dist*Math.cos(RA),
+	y: dist*Math.sin(RA)
+    };
+}
+
+function makeBodies(data, geocentric) {
+    initializeCanvas();
+    let bodies = [];
+    if (geocentric) {
+	for (var i = 0; i < data.table.rows.length; i++) {
+	    var object = data.table.rows[i].cells[0];
+	    if (object.name == "Earth") {
+		bodies.push({
+		    color: colorSwitch.Earth,
+		    radius: 5 / SCALE,
+		    x: 0,
+		    y: 0
+		})
+		continue
+	    }
+	    var objName = object.name;
+	    var objPos = earthFrameLoc(parseFloat(object.position.equatorial.rightAscension.hours), parseFloat(object.distance.fromEarth.au));
+	    var mag = parseFloat(object.extraInfo.magnitude);
+	    bodies.push({
+		color: colorSwitch.objName,
+		radius: 10**(mag/-2.5),
+		x: objPos.x,
+		y: objPos.y
+	    });
+	}
+    } else {
+	for (var i = 0; i < data.table.rows.length; i++) {
+	    var object = data.table.rows[i].cells[0];
+	    if (object.name == "Sun") {
+		bodies.push({
+		    color: colorSwitch.Sun,
+		    radius: 5 / SCALE,
+		    x: 0,
+		    y: 0
+		});
+		const sunPos = earthFrameLoc(parseFloat(object.position.equatorial.rightAscension.hours), parseFloat(object.distance.fromEarth.au));
+		const theSun = object;
+		break;
+	    }
+	}
+	for (var i = 0; i < data.table.rows.length; i++) {
+	    var object = data.table.rows[i].cells[0];
+	    if (object.name == "Sun") {
+		continue;
+	    }
+	    if (object.name == "Earth") {
+		bodies.push({
+		    color: colorSwitch.Earth,
+		    radius: 5 / SCALE,
+		    x: -sunPos.x,
+		    y: -sunPos.y
+		});
+		continue;
+	    }
+	    var objName = object.name;
+	    var objPos = earthFrameLoc(parseFloat(object.position.equatorial.rightAscension.hours), parseFloat(object.distance.fromEarth.au));
+	    var mag = parseFloat(object.extraInfo.magnitude);
+	    bodies.push({
+		color: colorSwitch.objName,
+		radius: 10**(mag/-2.5),
+		x: objPos.x - sunPos.x,
+		y: objPos.y - sunPos.y
+	    });
+	}
+    }
+    return bodies;
+}
+
+function drawBodies(data, geocentric) {
+    const bodies = makeBodies(data, geocentric);
+    for (var i = 0; i < bodies.length; i++) {
+	var body = bodies[i];
+	ctx.arc(body.x, body.y, body.radius / SCALE, 0, 2 * Math.pi);
+	ctx.strokeStyle = body.color;
+	ctx.stroke();
+	ctx.fillStyle = body.color;
+	ctx.fill();
+    }
+}
+
+function test(geocentric) {
     const date_time = document.getElementById("dateTime").value.toString();
-    const  date_time_array = date_time.split("T");
-    console.log(date_time_array);
-    GET(date_time_array[0], date_time_array[1]);
+    const date_time_array = date_time.split("T");
+    var data = GET(date_time_array[0], date_time_array[1]);
+    drawBodies(data, geocentric);
 }
