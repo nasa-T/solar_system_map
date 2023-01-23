debc3686387903b92d8a6a93af4f0992d1b74547
Tasan <tas2@williams.edu>
2023-01-23 10:57:22 -0500

    change query method

--- js/solar_system_map.js
+++ js/solar_system_map.js
@@ -1,27 +1,27 @@
-import axios from "./node_modules/axios";
+// import axios from "./node_modules/axios";
 
-const options = {
-  method: 'GET',
-  url: 'https://astronomy.p.rapidapi.com/api/v2/bodies/positions',
-  params: {
-    latitude: '33.775867',
-    longitude: '-84.39733',
-    from_date: '2017-12-20',
-    to_date: '2017-12-21',
-    elevation: '166',
-    time: '12:00:00'
-  },
-  headers: {
-    'X-RapidAPI-Key': '1948f0adcfmsha6c2f85eda754c3p1ebb93jsn77a08f207f83',
-    'X-RapidAPI-Host': 'astronomy.p.rapidapi.com'
-  }
-};
+// const options = {
+//   method: 'GET',
+//   url: 'https://astronomy.p.rapidapi.com/api/v2/bodies/positions',
+//   params: {
+//     latitude: '33.775867',
+//     longitude: '-84.39733',
+//     from_date: '2017-12-20',
+//     to_date: '2017-12-21',
+//     elevation: '166',
+//     time: '12:00:00'
+//   },
+//   headers: {
+//     'X-RapidAPI-Key': '1948f0adcfmsha6c2f85eda754c3p1ebb93jsn77a08f207f83',
+//     'X-RapidAPI-Host': 'astronomy.p.rapidapi.com'
+//   }
+// };
 
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
 
 // const options = {
 // 	method: 'GET',
@@ -36,17 +36,17 @@ axios.request(options).then(function (response) {
 // 	.then(response => console.log(response))
 // 	.catch(err => console.error(err));
 
-// const settings = {
-// 	"async": true,
-// 	"crossDomain": true,
-// 	"url": "https://astronomy.p.rapidapi.com/api/v2/bodies/positions?latitude=33.775867&longitude=-84.39733&from_date=2017-12-20&to_date=2017-12-21&elevation=166&time=12%3A00%3A00",
-// 	"method": "GET",
-// 	"headers": {
-// 		"X-RapidAPI-Key": "1948f0adcfmsha6c2f85eda754c3p1ebb93jsn77a08f207f83",
-// 		"X-RapidAPI-Host": "astronomy.p.rapidapi.com"
-// 	}
-// };
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://astronomy.p.rapidapi.com/api/v2/bodies/positions?latitude=42.7115&longitude=-73.2017&from_date=2023-1-23&to_date=2023-1-23&time=12%3A00%3A00",
+	"method": "GET",
+	"headers": {
+		"X-RapidAPI-Key": "1948f0adcfmsha6c2f85eda754c3p1ebb93jsn77a08f207f83",
+		"X-RapidAPI-Host": "astronomy.p.rapidapi.com"
+	}
+};
 
-// $.ajax(settings).done(function (response) {
-// 	console.log(response);
-// });
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
